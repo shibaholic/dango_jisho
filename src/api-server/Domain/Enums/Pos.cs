e3e7e44ae144aea76f14bc2396c273a56cb36eb0
shibaholic <>
2025-02-12 22:14:32 +0100

    pos

---
+++ src/api-server/Domain/Enums/Pos.cs
@@ -0,0 +1,6 @@
+namespace Domain.Enums;
+
+public enum Pos
+{
+    // I don't need to do this enum.... just map to enum on the client-side....
+}
--- src/client/src/pages/search/FatListCard.tsx
+++ src/client/src/pages/search/FatListCard.tsx
@@ -2,6 +2,8 @@ import { Entry } from "@/types/JMDict";
 import React from "react";
 import ObjectDisplay from "../testing/tracked-entries/objectDisplay";
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
+import { areStringArraysEqual } from "@/utils/stringUtils";
+import { posMapping } from "@/types/enums/Pos";
 
 interface FatListCardProps {
   entry: Entry;
@@ -13,64 +15,82 @@ const FatListCard = ({ entry }: FatListCardProps) => {
   if (entry.selectedKanjiIndex !== null) {
     faceTermElement = (
       <>
-        <p>
-          <ruby className="relative inline-block">
-            <h3 className="inline-block text-2xl font-bold">
-              {entry.kanjiElements[entry.selectedKanjiIndex].keb}
-            </h3>
+        <ruby className="relative inline-block">
+          <h3 className="inline-block text-3xl font-bold">
+            {entry.kanjiElements[entry.selectedKanjiIndex].keb}
+          </h3>
 
-            <rp className="hidden">(</rp>
+          <rp className="hidden">(</rp>
 
-            <rt className="absolute -top-4 left-0 text-xs">
-              {entry.readingElements[entry.selectedReadingIndex].reb}
-            </rt>
+          <rt className="absolute -top-6 left-1.5 text-lg">
+            {entry.readingElements[entry.selectedReadingIndex].reb}
+          </rt>
 
-            <rp className="hidden">)</rp>
-          </ruby>
-        </p>
+          <rp className="hidden">)</rp>
+        </ruby>
       </>
     );
   } else {
     faceTermElement = (
       <>
-        <h3 className="inline-block text-2xl font-bold">
+        <h3 className="inline-block text-3xl font-bold">
           {entry.readingElements[entry.selectedReadingIndex].reb}
         </h3>
       </>
     );
   }
 
+  function createPosSpans(pos: string[]): JSX.Element {
+    const poses = currentPos.map((pos_str, index) => {
+      let punctuation = ", ";
+      if (index === pos.length - 1) punctuation = "";
+      return (
+        <span className="font-light text-gray-800">
+          {posMapping[pos_str]}
+          {punctuation}
+        </span>
+      );
+    });
+    return <p>{poses}</p>;
+  }
+
+  let glossesSection: JSX.Element[] = [];
+
+  let currentPos = [""];
+
+  entry.senses.forEach((sense, index) => {
+    if (!areStringArraysEqual(currentPos, sense.pos)) {
+      currentPos = sense.pos;
+      glossesSection.push(<>{createPosSpans(currentPos)}</>);
+    }
+
+    glossesSection.push(
+      <p className="leading-loose">
+        <span className="font-light text-xl">{index}. </span>
+
+        {sense.gloss.map((gloss, index, array) => {
+          let punctuation = ", ";
+          if (index === array.length - 1) punctuation = "";
+          return (
+            <span className="font-medium text-xl">
+              {gloss}
+              {punctuation}
+            </span>
+          );
+        })}
+
+        <span className="font-light ml-4">{sense.s_inf} </span>
+      </p>
+    );
+  });
+
   return (
     <Card>
-      <CardHeader className="flex flex-row justify-start">
+      <CardHeader className="flex flex-row justify-start mt-2">
         <CardTitle>{faceTermElement}</CardTitle>
       </CardHeader>
       <CardContent>
-        <div>
-          {entry.senses.map((sense, index) => {
-            return (
-              <div>
-                <p>
-                  <span className="font-light">{index}. </span>
-                  {sense.gloss.map((gloss, index, array) => {
-                    let glossSpan = (
-                      <span className="font-medium text-lg">{gloss}; </span>
-                    );
-
-                    if (index === array.length - 1) {
-                      glossSpan = (
-                        <span className="font-medium text-lg">{gloss}</span>
-                      );
-                    }
-
-                    return <>{glossSpan}</>;
-                  })}
-                  <span className="font-light ml-4">{sense.s_inf} </span>
-                </p>
-              </div>
-            );
-          })}
-        </div>
+        <div>{glossesSection}</div>
       </CardContent>
     </Card>
   );
